4c26a4fa1d7dd2da8973086bcd5bed7e787eb833
mrniebels <mrniebels@gmail.com>
2020-02-07 22:23:52 +0100

    Initial commit

---
+++ BookListRazor/BookListRazor/BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListRazor
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db; // Dependency Injection.
+        }
+
+        public IEnumerable<Book> Books { get; set; }
+
+        public async Task OnGet() // await requires "public async Task" instead of "public void".
+        {
+            Books = await _db.Book.ToListAsync(); // Async and await, instead of while.
+        }
+    }
+}
